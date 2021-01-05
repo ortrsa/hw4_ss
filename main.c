6afd153bc64c1129246cbde4094789bb2dd9de6a
or trabelsi <ortrsa@gmail.com>
2021-01-05 10:05:17 +0200

    first

---
+++ main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Hw4_ss
+//
+//  Created by Or Trabelsi on 05/01/2021.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
